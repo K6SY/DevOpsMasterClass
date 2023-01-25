999666de9f12210e3ea83f82c2f86e8b4bab6396
Kalidou SY <kalidou.harouna.sy@gmail.com>
2023-01-25 00:39:54 +0000

    Lab-k8s

--- Containers/simple-php-mysql-app/app/index.php
+++ Containers/simple-php-mysql-app/app/index.php
@@ -12,13 +12,10 @@
           //These are the defined authentication environment in the db service
 
           // The MySQL service named in the docker-compose.yml.
-          $host = 'database';
 
-          // Database use name
-          $user = 'MYSQL_USER';
-
-          //database user password
-          $pass = 'MYSQL_PASSWORD';
+          $host = getenv('DATABASE_HOSTNAME');
+          $user = getenv('DATABASE_USERNAME');
+          $pass = getenv('DATABASE_PASSWORD');
 
           // check the MySQL connection status
           $conn = new mysqli($host, $user, $pass);
--- Containers/simple-php-mysql-app/app/student.php
+++ Containers/simple-php-mysql-app/app/student.php
@@ -10,16 +10,11 @@
         <?php
           //These are the defined authentication environment in the db service
 
-          // The MySQL service named in the docker-compose.yml.
-          $host = 'database';
-
-          // Database use name
-          $user = 'MYSQL_USER';
-
-          $mydatabase = 'MY_DATABASE';
-
-          //database user password
-          $pass = 'MYSQL_PASSWORD';
+          $host = getenv('DATABASE_HOSTNAME');
+          $user = getenv('DATABASE_USERNAME');
+          $pass = getenv('DATABASE_PASSWORD');
+          $mydatabase = getenv('DATABASE_NAME');
+          
 
           // check the MySQL connection status
           $conn = new mysqli($host, $user, $pass, $mydatabase);
